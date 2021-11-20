1295c2f525bac828b1b4b5845ef05134f0d84847
rivoPelu <contactrivopelu@gmail.com>
2021-11-20 23:34:12 +0800

    active form

--- js/main.js
+++ js/main.js
@@ -68,7 +68,8 @@ tabs.forEach(tab => {
         })
     })
     /*==================== SERVICES MODAL ====================*/
-const modalViews = document.querySelectorAll('.services__modal'),
+const modalViews = document.querySelectorAll('.services__Kirim'),
+
     modalBtns = document.querySelectorAll('.services__button'),
     modalCloses = document.querySelectorAll('.services__modal-close')
 
@@ -94,19 +95,19 @@ modalCloses.forEach((modalClose) => {
 /*==================== PORTFOLIO SWIPER  ====================*/
 let swiper = new Swiper('.swiper-container', {
     cssMode: true,
-    loop:true,
-    
+    loop: true,
+
     navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
 
 
     pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
+        el: '.swiper-pagination',
+        clickable: true,
     },
-    
+
 });
 
 /*==================== TESTIMONIAL ====================*/
@@ -115,17 +116,17 @@ let swiper = new Swiper('.swiper-container', {
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('section[id]')
 
-function scrollActive(){
+function scrollActive() {
     const scrollY = window.pageYOffset
 
-    sections.forEach(current =>{
+    sections.forEach(current => {
         const sectionHeight = current.offsetHeight
         const sectionTop = current.offsetTop - 50;
         sectionId = current.getAttribute('id')
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
+        } else {
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
         }
     })
@@ -133,18 +134,20 @@ function scrollActive(){
 window.addEventListener('scroll', scrollActive)
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/
-function scrollHeader(){
+function scrollHeader() {
     const nav = document.getElementById('header')
-    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+        // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    if (this.scrollY >= 80) nav.classList.add('scroll-header');
+    else nav.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
 /*==================== SHOW SCROLL UP ====================*/
-function scrollUp(){
+function scrollUp() {
     const scrollUp = document.getElementById('scroll-up');
     // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
-    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+    if (this.scrollY >= 560) scrollUp.classList.add('show-scroll');
+    else scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
 
@@ -163,9 +166,9 @@ const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-mo
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
 }
 
 // Activate / deactivate the theme manually with the button
@@ -173,7 +176,37 @@ themeButton.addEventListener('click', () => {
     // Add or remove the dark / icon theme
     document.body.classList.toggle(darkTheme)
     themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
+        // We save the theme and the current icon that the user chose
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+
+
+// google form
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbx9voEhuZ4zgo7OMSjhJ4kMSgcUbUobU5jQTHzQAvdW2glc6qdDiIPOmBRFff9sqd4-Gg/exec'
+const form = document.forms['Form-Contact-Rivo-Web']
+const loading = document.querySelector('.loading')
+const kirim = document.querySelector('.kirim')
+const close = document.querySelector('.kirim__close')
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    // ketika di kirim tampilkan loading 
+    loading.classList.add('active-loading');
+    kirim.classList.remove('active-loading');
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            loading.classList.remove('active-loading');
+            kirim.classList.add('active-kirim');
+            if (close) {
+                close.addEventListener('click', () => {
+                    kirim.classList.remove('active-kirim')
+                })
+            }
+            form.reset();
+            console.log('Success!', response)
+        })
+        .catch(error => console.error('Error!', error.message))
 })
